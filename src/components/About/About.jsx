42692aa6e14f312934bc8e6bbf712c0ae4444fe0
NishatFariza <nishatfarzia78@gmail.com>
2022-03-26 19:57:16 +0600

    about add

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import './About.css'
+import { SiHashnode } from 'react-icons/si'
 
 const About = () => {
     return (
-        <div >
+        <div className='about-wrapper' >
             <h2>About React</h2>
-            <div>
-                <h4>How React Works?</h4>
-                <p><strong>Ans:</strong><span>React.js is one of the most talked about JavaScript web frameworks in years.</span></p>
+            <div className='about-item'>
+                <h3 className='about-question'><SiHashnode className='question-icon'></SiHashnode>How React Works?</h3>
+                <h4 className='strong-text'>Ans:  <span className='about-ans'>React.js is one of the most talked about JavaScript web frameworks in years.
+                js is one of the most talked about JavaScript web frameworks in years.</span></h4>
+            </div>
+            <div className='about-item'>
+                <h3 className='about-question'><SiHashnode className='question-icon'></SiHashnode>How React Works?</h3>
+                <h4 className='strong-text'>Ans:  <span className='about-ans'>React.js is one of the most talked about JavaScript web frameworks in years.</span></h4>
+            </div>
+            <div className='about-item'>
+                <h3 className='about-question'><SiHashnode className='question-icon'></SiHashnode>How React Works?</h3>
+                <h4 className='strong-text'>Ans:  <span className='about-ans'>React.js is one of the most talked about JavaScript web frameworks in years.</span></h4>
             </div>
         </div>
     );
