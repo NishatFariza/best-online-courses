c798ff2f2457d3fe16ce1cbe331ee95056c16e96
NishatFariza <nishatfarzia78@gmail.com>
2022-03-26 20:19:30 +0600

    style add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/About/About";
 import Courses from "./components/Courses/Courses";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header></Header>
       <Courses></Courses>
       <About></About>
+      <Footer></Footer>
     </div>
   );
 }
